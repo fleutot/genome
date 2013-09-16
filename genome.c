24e2bf688eabd82b9232b36c9f3dbab859ca7c6d
Gauthier Östervall <gauthier@ostervall.se>
2013-09-16 13:55:37 +0200

    Sanity check for genomes.
    Correct the generation of random commands (operation and operands
    were not wrapped and got out of range).

--- genome.c
+++ genome.c
@@ -34,6 +34,9 @@ typedef struct {
 
 // This typedef sets the max size of the genome. uint16_t sets the max to 65535,
 // for example. uint8_t sets the max to 255.
+// It is not currently possible to have a max that is not a power of two (not
+// having a power of two would result in a skew in the random distribution. Not
+// a big problem, but still).
 typedef uint8_t genome_size_t;
 
 
@@ -46,12 +49,15 @@ typedef uint8_t genome_size_t;
 // Module variables
 //******************************************************************************
 static bool randomizer_seeded = false;
+static bool gene_was_valid = false;
 
 
 //******************************************************************************
 // Function prototypes
 //******************************************************************************
-void gene_destroy(void const * const data);
+static void gene_destroy(void const * const data);
+static void gene_display(void const * const data);
+static void gene_valid_check(void const * const data);
 
 
 //******************************************************************************
@@ -75,25 +81,39 @@ genome_t *genome_random_create(void)
         randomizer_seeded = true;
     }
 
-    // These uses of rand() are somewhat skewed (non uniform). It is ok in this
-    // application, since the sizes of the data types are small and the skew is
-    // small.
     genome_size_t genome_size = rand();
-
-    #if defined(GENOME_DEBUG)
-    printf("Size of new genome: %d\n", genome_size);
-    #endif // GENOME_DEBUG
 
     for (genome_size_t i = 0; i < genome_size; i++) {
         command_t *new_command_p = malloc(sizeof (command_t));
-        new_command_p->dst = (register_t) rand();
-        new_command_p->op  = (operation_t) rand();
-        new_command_p->src1 = (register_t) rand();
-        new_command_p->src2 = (register_t) rand();
+        // Modulo of a random number with a number that is not a power of two is
+        // no uniformly random, but that is not very important for this
+        // application.
+        new_command_p->dst = (register_t) rand() % NB_REGISTERS;
+        new_command_p->op  = (operation_t) rand() % NB_OPERATION_TYPES;
+        new_command_p->src1 = (register_t) rand() % NB_REGISTERS;
+        new_command_p->src2 = (register_t) rand() % NB_REGISTERS;
         linkedlist_add(new_genome_p->genes, new_command_p);
     }
 
+    if (!genome_sanity_check(new_genome_p)) {
+        fprintf(stderr, "%s: new genome is corrupt.\n", __func__);
+    }
+
     return new_genome_p;
+}
+
+
+//  ----------------------------------------------------------------------------
+/// \brief  Check if the genes of genome look right, by looking at the data
+/// itself. So far this only checks if the elements of command are in range.
+/// \param  genome Pointer to the genome to check.
+/// \return True if the genom seems valid.
+//  ----------------------------------------------------------------------------
+bool genome_sanity_check(genome_t const * const genome)
+{
+    gene_was_valid = true;
+    linkedlist_run_for_all(genome->genes, gene_valid_check);
+    return gene_was_valid;
 }
 
 
@@ -120,10 +140,76 @@ void genome_destroy(genome_t *genome)
 //******************************************************************************
 // Internal functions
 //******************************************************************************
-void gene_destroy(void const * const data)
+//  ----------------------------------------------------------------------------
+/// \brief  Deallocate the data for a gene. This must be done here, since the
+/// linkedlist used to sequence the genes does not own the genes themselves. To
+/// be used as callback to linkedlist_run_for_all().
+/// \param  data Pointer to the gene to deallocate.
+//  ----------------------------------------------------------------------------
+static void gene_destroy(void const * const data)
 {
-    // This is deliberate, killing data in spite of constness.  Other callbacks
-    // to linkedlist should not touch data, destroy is an exception. This may
-    // change in the future, to allow for genetic mutations for example.
+    // The typecast is deliberate, killing data in spite of constness.  Other
+    // callbacks to linkedlist should not touch data, destroy is an
+    // exception. This may change in the future, to allow for genetic mutations
+    // for example.
     free((void *) data);
+}
+
+
+//  ----------------------------------------------------------------------------
+/// \brief  Display the command gene passed as parameter on standard output. To
+/// be used as callback to linkedlist_run_for_all().
+/// \param  data Pointer to the gene to display.
+//  ----------------------------------------------------------------------------
+static void gene_display(void const * const data)
+{
+    static const char *reg_to_string[NB_REGISTERS] = {
+        [A] = "A", [B] = "B", [C] = "C", [D] = "D", [E] = "E", [F] = "F",
+        [G] = "G", [H] = "H", [I] = "I", [J] = "J", [K] = "K", [L] = "L",
+        [M] = "M", [N] = "N", [O] = "O", [P] = "P"
+    };
+
+    static const char *op_to_string[NB_OPERATION_TYPES] = {
+        [ADD] = "+", [SUB] = "-", [MUL] = "*", [DIV] = "/"
+    };
+
+    command_t *command = (command_t *) data;
+
+    #if defined(GENOME_DEBUG)
+    gene_was_valid = true;
+    gene_valid_check(data);
+    if (!gene_was_valid) {
+        fprintf(stderr, "%s: gene member out of range.\n", __func__);
+        return;
+    }
+    #endif // GENOME_DEBUG
+
+    printf("gene %s = (%s %s %s)\n",
+           reg_to_string[command->dst],
+           op_to_string[command->op],
+           reg_to_string[command->src1],
+           reg_to_string[command->src2]
+        );
+}
+
+
+//  ----------------------------------------------------------------------------
+/// \brief  Check if the gene passed as parameter seems valid. So far it just
+/// checks if operation and operands are in range. To be used as callback to
+/// linkedlist_run_for_all().
+/// \param  data Pointer to the gene to check.
+/// \return The result is stored in the module variable gene_was_valid, which
+/// needs to be reset to true before checking a new genome.
+//  ----------------------------------------------------------------------------
+static void gene_valid_check(void const * const data)
+{
+    command_t *command = (command_t *) data;
+
+    if (command->dst < 0 || command->dst >= NB_REGISTERS
+        || command->op < 0 || command->op >= NB_OPERATION_TYPES
+        || command->src1 < 0 || command->src1 >= NB_REGISTERS
+        || command->src2 < 0 || command->src2 >= NB_REGISTERS
+        ) {
+        gene_was_valid = false;
+    }
 }
--- genome.h
+++ genome.h
@@ -5,6 +5,8 @@ Copyright (c) 2013 Gauthier Fleutot Östervall
 #ifndef GENOME_H_INCLUDED
 #define GENOME_H_INCLUDED
 
+#include <stdbool.h>
+
 typedef struct genome_s genome_t;
 
 
@@ -20,6 +22,13 @@ genome_t *genome_random_create(void);
 /// \param  genome The genome to free.
 //  ----------------------------------------------------------------------------
 void genome_destroy(genome_t *genome);
+
+
+//  ----------------------------------------------------------------------------
+/// \brief  Check if the genome passed as parameter looks right.
+/// \param  True if the genome seems valid.
+//  ----------------------------------------------------------------------------
+bool genome_sanity_check(genome_t const * const genome);
 
 
 #endif // GENOME_H_INCLUDED
